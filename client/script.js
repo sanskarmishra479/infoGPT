0edf6e55998eb93677034e649d063c71f7f3696c
Sanskar Mishra <135611603+sanskarmishra479@users.noreply.github.com>
2024-12-02 10:32:02 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('https://infogpt-2.onrender.com', {
+    const response = await fetch('https://infogpt-3.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
