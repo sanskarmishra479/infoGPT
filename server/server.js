568a9a5bbfc455f0ca2c700b5699155e761004a5
Sanskar Mishra <sanskarmishra479@gmail.com>
2024-12-13 14:19:42 +0000

    change gemini model

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash-exp",
 });
 
 const generationConfig = {
